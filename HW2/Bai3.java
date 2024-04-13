c1ecca87bd2ca52e8591186874194759a9f60dc5
Khanhtoichoi <23020386@vnu.edu.vn>
2024-04-13 18:17:29 +0700

    Create Bai3.java

---
+++ HW2/Bai3.java
@@ -0,0 +1,22 @@
+package OOP.HW2;
+
+import java.awt.*;
+
+public class Bai3 {
+    public static void main(String[] args){
+        Picture picture = new Picture(args[0]);
+        int x = picture.width();
+        int y = picture.height();
+        Picture moi = new Picture(x,y);
+
+        for(int j=0; j<x; j++){
+            for(int i=0; i<y/2; i++){
+                Color color = picture.get(i,j);
+                Color color1 = picture.get(x-i-1,j);
+                moi.set(i,j,color1);
+                moi.set(x-i-1,j,color);
+            }
+        }
+        moi.show();
+    }
+}
