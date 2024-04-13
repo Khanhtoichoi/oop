e83cfa503583e067e0216824decd4207893ce9d2
Khanhtoichoi <23020386@vnu.edu.vn>
2024-04-13 10:11:01 +0700

    Create Bai1.java

---
+++ HW2/Bai1.java
@@ -0,0 +1,19 @@
+package OOP.HW2;
+
+public class Bai1 {
+    public static String reverse(String s){
+        int x = s.length();
+        char[] a = s.toCharArray();
+        for(int i=0; i<x/2; i++){
+            char k = a[i];
+            a[i] = a[x-i-1];
+            a[x-i-1] = k;
+        }
+        return new String(a);
+    }
+    public static void main(String[] args){
+        String s = args[0];
+        String moi = reverse(s);
+        System.out.print(moi);
+    }
+}
