f675d958544cb51d3dd6c8eb1692d10c688a9cd7
Khanhtoichoi <23020386@vnu.edu.vn>
2024-04-14 12:01:53 +0700

    Update Point.java

--- HW2/Point.java
+++ HW2/Point.java
@@ -1,4 +1,18 @@
 package OOP.HW2;
 
 public class Point {
+    private double x;
+    private double y;
+    Point(double a, double b){
+        this.x = a;
+        this.y = b;
+    }
+    public double distance(Point q){
+        double dx = this.x - q.x;
+        double dy = this.y - q.y;
+        return(Math.sqrt(dx*dx+dy*dy));
+    }
+    public String toString(){
+        return "("+x+","+y+")";
+    }
 }
