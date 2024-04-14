65c9076594b03f5ee47124d77603812862b42286
Khanhtoichoi <23020386@vnu.edu.vn>
2024-04-14 22:25:01 +0700

    Create RollDie.java

---
+++ HW1/RollDie.java
@@ -0,0 +1,9 @@
+package OOP.HW1;
+
+public class RollDie {
+    public static void main(String[] args){
+        int x = 6;
+        int y = (int)(Math.random()*x)+1;
+        System.out.print(y);
+    }
+}
