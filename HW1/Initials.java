bdd35dc7e1d80f669baf6df3c638d4a9cb348ff2
Khanhtoichoi <23020386@vnu.edu.vn>
2024-04-14 12:16:24 +0700

    Create Initials.java

---
+++ HW1/Initials.java
@@ -0,0 +1,15 @@
+package OOP.HW1;
+
+public class Initials {
+    public static void main(String[] args){
+        System.out.println("**        ***    **********      **             *             **");
+        System.out.println("**      ***      **        **     **           ***           ** ");
+        System.out.println("**    ***        **         **     **         ** **         **  ");
+        System.out.println("**  ***          **          **     **       **   **       **   ");
+        System.out.println("*****            **          **      **     **     **     **    ");
+        System.out.println("**  ***          **          **       **   **       **   **     ");
+        System.out.println("**    ***        **         **         ** **         ** **      ");
+        System.out.println("**      ***      **        **           ***           ***       ");
+        System.out.println("**        ***    **********              *             *        ");
+    }
+}
