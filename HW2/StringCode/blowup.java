9d7b56cc634e675420994a3967cc9dcf99068a3c
Khanhtoichoi <23020386@vnu.edu.vn>
2024-04-21 23:58:57 +0700

    Create blowup.java

---
+++ HW2/StringCode/blowup.java
@@ -0,0 +1,21 @@
+package OOP.HW2.StringCode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class blowup {
+    public static String Blowup(String s){
+        char[] a = s.toCharArray();
+        String ss = "";
+        for(int i=0; i<a.length-1; i++){
+            if(Character.isDigit(a[i])){
+                int tmp = a[i]-'0';
+                for(int j=0; j<tmp; j++){
+                    ss = ss+a[i+1];
+                }
+            }
+            else ss = ss + a[i];
+        }
+        if(!Character.isDigit(a[a.length-1])) ss = ss+a[a.length-1];
+        return ss;
+    }
+}
